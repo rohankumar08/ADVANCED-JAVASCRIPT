0a8afe6c511e882405bdca83afc33182830a9d38
rohankumar08 <rohansahu1234ssm@gmail.com>
2025-04-29 12:58:13 +0530

    Rest and js exponentialand array include and Trailing comma

--- map.js
+++ map.js
@@ -68,18 +68,74 @@
 
 
 //***********************DESTRUCTURING***************************** */
- const person={
-    firstName:'john',
-    lastName:'Doe',
-    age:50
- };
- //destructuring:-it doesn't change original copy
+//  const person={
+//     firstName:'john',
+//     lastName:'Doe',
+//     age:50
+//  };
+//  //destructuring:-it doesn't change original copy
 
- let{firstName,lastName,country="INDIA"}=person;  //order doen't matter  // country is a default value
- console.log(firstName);
- console.log(lastName);
- console.log(country);
+//  let{firstName,lastName,country="INDIA"}=person;  //order doen't matter  // country is a default value
+//  console.log(firstName);
+//  console.log(lastName);
+//  console.log(country);
 
- //  allias name creation
- let{lastName : name}=person;
- console.log(name);
+//  //  allias name creation
+//  let{lastName : name}=person;
+//  console.log(name);
+
+//  let name="Gurukultheschool";
+//  let[a1,a2,a3,a4,a5]=name;
+//  console.log(a1);
+
+// const fruits=["Banana","Oranges","Apple","Mangoes"];
+// // let[fruit1,fruit2]=fruits;
+// // console.log(fruit1);
+// // console.log(fruit2);
+// let{[0]:fruit1,[3]:fruit2}=fruits;
+// console.log(fruit1);
+// console.log(fruit2);
+
+//**************************Rest********************************** */
+// const number=[1,2,3,4,5,6,7];
+// const[a,b,...Rest]=number;
+// console.log(a);
+// console.log(b);
+// console.log(Rest);
+
+// let firstName="Rohan";
+// let lastName="Kumar";
+// [firstName,lastName]=[lastName,firstName];
+// console.log(firstName);
+// console.log(lastName);
+
+//*****************************js exponentiation********************** */
+// let x=5;
+// console.log(x**2);
+// console.log(Math.pow(x,2));// library
+// console.log(x**=2);  //x=25
+
+
+//******************js Array includes***************** */
+// const fruits=["Banana","Oranges","Apple","Mangoes"];
+// console.log(fruits.includes("Mangoes"));
+// console.log(fruits.includes("Banana",3)); // here 3 is starting postipn of searching //false
+
+
+//***************TRAILING COMMA*************************** ************************/
+// const arr=["one","Two","Three",,];
+// console.log(arr.length);
+// console.log(arr);
+
+// const person={
+//    firstName:"ROhan",
+//    lastName:"Kumar",
+//    age:20,
+// }
+// we cant create sparse onbect it is error(syntax error)
+// const person={
+//    firstName:"ROhan",
+//    lastName:"Kumar",
+//      ,
+//    age:20,
+// }
